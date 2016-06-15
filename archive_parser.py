71b8f29e67ff9213684115fc726d2c1336e09409
John <john@ustudio.com>
2016-06-15 00:59:19 -0500

    Handled parsing date edge cases.

--- archive_parser.py
+++ archive_parser.py
@@ -4,7 +4,8 @@ import tarfile
 import re
 
 HEADER_PATTERNS = {
-    "date": re.compile("Date: (.*)")
+    "date": re.compile("Date:\s*(.*)", flags=re.IGNORECASE),
+    "continuation": re.compile("^\s+(.*)")
 }
 
 
@@ -12,7 +13,7 @@ class InvalidArchiveFile(Exception):
     pass
 
 
-def extract_header_iterator(archive, archive_file):
+def parse_header_iterator(archive, archive_file):
     extracted_file = archive.extractfile(archive_file)
     for line in extracted_file:
         if line == b"\n":
@@ -20,19 +21,31 @@ def extract_header_iterator(archive, archive_file):
         yield line
 
 
+def parse_line_for_header(line):
+    for header, pattern in HEADER_PATTERNS.iteritems():
+        match = pattern.match(line)
+        if match:
+            return header, match
+
+    return None, None
+
+
 def parse_extracted_file(archive, archive_file):
-    is_file = archive_file.isfile()
-    if is_file:
-        parsed_headers = {}
-        for line in extract_header_iterator(archive, archive_file):
-            date = HEADER_PATTERNS["date"].match(line)
-            if date:
-                parsed_headers["date"] = date.group(1)
+    parsed_headers = {}
+    last_parsed_header = ""
+    for line in parse_header_iterator(archive, archive_file):
+        header, match = parse_line_for_header(line)
+        if match:
+            if header == "continuation":
+                parsed_headers[last_parsed_header] += match.group(1)
+            else:
+                parsed_headers[header] = match.group(1)
+                last_parsed_header = header
 
-        return parsed_headers
+    return parsed_headers
 
 
-def archive_extractor(archive_file_path):
+def parse_archive(archive_file_path):
     if not tarfile.is_tarfile(archive_file_path):
         raise InvalidArchiveFile("File is not a tar archive file.")
 
@@ -40,7 +53,9 @@ def archive_extractor(archive_file_path):
 
     with tarfile.open(archive_file_path) as archive:
         for archive_file in archive.getmembers():
-            results.append(parse_extracted_file(archive, archive_file))
+            is_file = archive_file.isfile()
+            if is_file:
+                results.append(parse_extracted_file(archive, archive_file))
 
     return results
 
@@ -57,7 +72,7 @@ def main():
     if not os.path.isfile(args.archive):
         raise InvalidArchiveFile("{} is not a valid file path.".format(args.archive))
 
-    archive_extractor(args.archive)
+    parse_archive(args.archive)
 
 if __name__ == "__main__":
     main()
--- tests/test_archive_parser.py
+++ tests/test_archive_parser.py
@@ -5,7 +5,7 @@ import tempfile
 import tarfile
 import os
 
-from archive_parser import main, InvalidArchiveFile, archive_extractor
+from archive_parser import main, InvalidArchiveFile, parse_archive
 
 _ORIGINAL_SYS_EXIT = sys.exit
 
@@ -18,39 +18,43 @@ class TestArchiveParser(unittest.TestCase):
     def setUp(self):
         sys.exit = self._check_sys_exit
         self.temp_files = []
+        self.default_entries = {
+            "date": "Date: Fri, 01 Apr 2011 05:52:55 PDT\n",
+            "from": "From: Corel <news@email1-corel.com>\n",
+            "subject": "Subject: PREVIEW:   Save $170 and get special gift with CorelDraw Premium"
+            "Suite X5\n"
+        }
 
     def tearDown(self):
         sys.exit = _ORIGINAL_SYS_EXIT
         for temp_file in self.temp_files:
             os.remove(temp_file)
 
-    def get_test_archive_file(self):
+    def get_test_archive_file(self, parsed_entries={}, extra_lines=[]):
         temp_directory = tempfile.mkdtemp()
 
-        temp_file_one = tempfile.NamedTemporaryFile(dir=temp_directory)
-        temp_file_one.write(
-            "Date: Fri, 01 Apr 2011 05:52:55 PDT\n"
-            "From: Corel <news@email1-corel.com>\n"
-            "Subject: PREVIEW:   Save $170 and get special gift with CorelDraw Premium Suite X5\n"
-        )
-        temp_file_one.flush()
+        default_entries = {}
+        default_entries.update(self.default_entries)
+        default_entries.update(parsed_entries)
+        required_entries = "".join([
+            "{date}".format(date=default_entries["date"]),
+            "{from_header}".format(from_header=default_entries["from"]),
+            "{subject}".format(subject=default_entries["subject"]),
+            "{extra_lines}".format(extra_lines="".join(extra_lines))
+        ])
 
-        temp_file_two = tempfile.NamedTemporaryFile(dir=temp_directory)
-        temp_file_two.write(
-            "Date: Fri, 01 Apr 2011 05:52:55 PDT\n"
-            "From: Corel <news@email1-corel.com>\n"
-            "Subject: PREVIEW:   Save $170 and get special gift with CorelDraw Premium Suite X5\n"
-        )
-        temp_file_two.flush()
+        temp_file_one = tempfile.NamedTemporaryFile(dir=temp_directory)
+        temp_file_one.write(required_entries)
+        temp_file_one.flush()
 
         temp_tar_file = tempfile.mkstemp()
         temp_tar_file_name = temp_tar_file[1]
 
         self.temp_files.append(temp_tar_file_name)
 
-        archive_file = tarfile.open(temp_tar_file_name, "w")
-        archive_file.add(temp_file_one.name)
-        archive_file.close()
+        with tarfile.open(temp_tar_file_name, "w") as archive_file:
+            archive_file.add(temp_file_one.name)
+            archive_file.close()
 
         return archive_file
 
@@ -71,20 +75,56 @@ class TestArchiveParser(unittest.TestCase):
         with self.assertRaises(InvalidArchiveFile):
             main()
 
-    @mock.patch("archive_parser.archive_extractor")
+    @mock.patch("archive_parser.parse_archive")
     def test_archive_parser_passes_correct_file_path_to_extractor(self, mock_extractor):
         temp_file = tempfile.NamedTemporaryFile()
         sys.argv = ["archive_parser.py", temp_file.name]
         main()
         mock_extractor.assert_called_with(temp_file.name)
 
-    def test_archive_extractor_raises_on_non_tar_file(self):
+    def test_parse_archive_raises_on_non_tar_file(self):
         temp_file = tempfile.NamedTemporaryFile()
         with self.assertRaises(InvalidArchiveFile):
-            archive_extractor(temp_file.name)
+            parse_archive(temp_file.name)
 
-    def test_archive_extractor_returns_correct_date(self):
+    def test_parsing_returns_correct_date(self):
         test_archive_file = self.get_test_archive_file()
-        result = archive_extractor(test_archive_file.name)
+        result = parse_archive(test_archive_file.name)
 
         self.assertEqual([{"date": "Fri, 01 Apr 2011 05:52:55 PDT"}], result)
+
+    def test_parseing_date_ignores_case(self):
+        test_archive_file = self.get_test_archive_file(
+            parsed_entries={"date": "date: Fri, 01 Apr 2011 05:52:55 PDT\n"})
+        result = parse_archive(test_archive_file.name)
+
+        self.assertEqual([{"date": "Fri, 01 Apr 2011 05:52:55 PDT"}], result)
+
+    def test_parsing_date_accepts_no_space(self):
+        test_archive_file = self.get_test_archive_file(
+            parsed_entries={"date": "date:Fri, 01 Apr 2011 05:52:55 PDT\n"})
+        result = parse_archive(test_archive_file.name)
+
+        self.assertEqual([{"date": "Fri, 01 Apr 2011 05:52:55 PDT"}], result)
+
+    def test_parsing_date_accepts_extra_space(self):
+        test_archive_file = self.get_test_archive_file(
+            parsed_entries={"date": "date:      Fri, 01 Apr 2011 05:52:55 PDT\n"})
+        result = parse_archive(test_archive_file.name)
+
+        self.assertEqual([{"date": "Fri, 01 Apr 2011 05:52:55 PDT"}], result)
+
+    def test_parsing_date_accepts_multiple_lines(self):
+        test_archive_file = self.get_test_archive_file(
+            parsed_entries={"date": "date: Fri, 01 Apr 2011 \n\t\t05:52:55 PDT\n"})
+        result = parse_archive(test_archive_file.name)
+
+        self.assertEqual([{"date": "Fri, 01 Apr 2011 05:52:55 PDT"}], result)
+
+    def test_parsing_ignores_body(self):
+        test_archive_file = self.get_test_archive_file(
+            parsed_entries={"date": ""},
+            extra_lines=["\n", "date: Fri, 02 Apr 2011 \n\t\t05:52:55 PDT\n"])
+        result = parse_archive(test_archive_file.name)
+
+        self.assertEqual([{}], result)
